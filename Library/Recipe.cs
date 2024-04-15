66061bf749daffc39733021a2d00b609c8195e07
Santifer0 <santiago.fc@correo.ucu.edu.uy>
2024-04-15 20:05:39 -0300

    EjSolucionado

---
+++ Library/Recipe.cs
@@ -0,0 +1,51 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Recipe
+    {
+        private ArrayList steps = new ArrayList();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Step step)
+        {
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+
+        public double GetProductionCost()
+        {
+            double total = 0;
+            foreach (Step step in this.steps)
+            {
+               total = total + step.Quantity * step.Input.UnitCost;
+               total = total + (step.Time / 60.00) * step.Equipment.HourlyCost;
+            }
+            return total;
+        }
+
+
+        public void PrintRecipe()
+        {
+            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            foreach (Step step in this.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+}
